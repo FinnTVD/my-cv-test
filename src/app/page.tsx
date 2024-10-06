aacc4940c45a6000ffbf9cca0fecd2a2b01ac231
FinnTVD <finnit.th@gmail.com>
2024-10-06 16:00:28 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,51 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/role-supports-aria-props */
+
+import BoxEditor from "@/components/BoxEditor";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex space-x-[2rem]">
+      <div className="w-[20%] sticky top-0 left-0 flex flex-col space-y-[1rem] h-fit shadow-lg items-center ">
+        <Link href={"#section1"}>Thông Tin</Link>
+        <Link href={"#section2"}>Overview</Link>
+        <Link href={"#section3"}>Work experience</Link>
+      </div>
+      <div className="w-[70%]">
+        <section className="h-[500px]"></section>
+        <div className="h-[500px]">
+          <BoxEditor id="section1" content="Trinh Van Duc" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="h-fit mb-[100px]">
+          <BoxEditor
+            spellCheck={true}
+            id="section2"
+            content="- Over 2 years of experience in programming with good communication
+              and quick learning skills - Strengths: Front-end technology and
+              Back-end web application development - Proficiency in HTML, CSS,
+              JavaScript - Strong proficiency in JavaScript, including DOM
+              manipulation and the JavaScript object model - Thorough under
+              standing of React.js and it score principles - Experience with
+              popular React.js workflows (such as Flux or Redux) - Familiarity
+              with newer specifications of EcmaScript - Experience with data
+              structure libraries - Familiarity with RESTful APIs - Strong
+              Experience in: PHP, JavaScript (ReactJS, React-native), MySQL,
+              NoSQL, GraphQL, Redis, JSON, API, Docker, Kubernetes, Rancher, AWS
+              services - Proficient use of source code management tools: SVN, GIT
+              - Proficiency in operating systems: Linux (Ubuntu, OSX), Windows -
+              Ability to learn and apply new technology quickly - Current working
+              location: Ha Noi, Viet Nam"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        </div>
+        <div className="h-screen">
+          <BoxEditor
+            spellCheck={true}
+            id="section3"
+            content="Work experience"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+    </main>
   );
 }
