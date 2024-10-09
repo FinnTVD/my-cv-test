cfa27b7bfe04ddb7ef90701f00a66814c2f24dc3
FinnTVD <finnit.th@gmail.com>
2024-10-10 01:45:06 +0700

    fix build

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,4 +1,8 @@
-import IndexDemo from "@/sections/demo/IndexDemo";
+import dynamic from "next/dynamic";
+
+const IndexDemo = dynamic(() => import("@/sections/demo/IndexDemo"), {
+  ssr: false,
+});
 
 const Page = () => {
   return <IndexDemo />;
